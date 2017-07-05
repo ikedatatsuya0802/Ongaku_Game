32d7d9ce252d0f598770de4fc2b15c15f8afcd8d
ikedatatsuya0802 <ikedatatsuya.0802@gmail.com>
2017-07-05 15:52:35 +0900

    first commit

---
+++ Ongaku_Game/Assets/script/TouchScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GodTouches;
+
+public class TouchScript : MonoBehaviour {
+
+	public GameObject go;
+	Vector3 pos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		pos = go.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(true)
+		{
+			pos.z += 1.0f;
+			go.transform.position = pos;
+		}
+	}
+}
